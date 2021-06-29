38558cfe6431c26ef3fa9014bc1208687c8347c4
DVL_KinoMan <dvali.daviti@gmail.com>
2021-06-29 18:38:02 +0400

    film-person dialog edit

--- src/app/films/film-edit/film-person/film-person.dialog.component.ts
+++ src/app/films/film-edit/film-person/film-person.dialog.component.ts
@@ -1,11 +1,10 @@
-import { Component, Inject } from "@angular/core";
+import { Component, ElementRef, Inject, ViewChild } from "@angular/core";
 import { FormControl } from "@angular/forms";
 import { MatAutocompleteSelectedEvent } from "@angular/material/autocomplete";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
 import { Observable } from "rxjs";
-import { debounceTime, map, startWith, switchMap } from "rxjs/operators";
+import { debounceTime, startWith, switchMap } from "rxjs/operators";
 import { Photo } from "src/app/common/photo";
-import { PhotosService } from "src/app/common/services/photos.service";
 import { Gender } from "src/app/persons/enums";
 import { Person } from "src/app/persons/person";
 import { PersonsService } from "src/app/persons/services/persons.service";
@@ -18,14 +17,16 @@ import { PersonsService } from "src/app/persons/services/persons.service";
 
 export class FilmPersonDialogComponent {
     filteredPersons: Observable<Person[]>;
-    model: Person;
+    model: Person | undefined;
     personsCtrl = new FormControl();
     showPersons = 10;
+    canEdit: boolean = true;
+
+    @ViewChild('personInput') personInput: ElementRef<HTMLInputElement>;
 
     constructor(
         public dialogRef: MatDialogRef<FilmPersonDialogComponent>,
         private personsService: PersonsService,
-        private photosService: PhotosService,
         @Inject(MAT_DIALOG_DATA) public data: DialogData
     ) {
         this.filteredPersons = this.personsCtrl.valueChanges.pipe(
@@ -41,41 +42,13 @@ export class FilmPersonDialogComponent {
         else event.target.src = person.sex == Gender.Female ? 'assets/DefaultPersonFemale.png' : 'assets/DefaultPersonMale.png'
     }
 
-    // selectedPerson(event: MatAutocompleteSelectedEvent): void {
-    //     this.addGenreToModel(event.option.viewValue);
-    //     this.genreInput.nativeElement.value = '';
-    //     this.genreCtrl.setValue(null);
-    // }
-
-    // addGenreToModel(value: string) {
-    //     var genre = this.genres.find(g => g.name == value);
-
-    //     if (!genre || this.model.genres?.find(g => g.id == genre?.id))
-    //         return;
-
-    //     if (!this.model.genres)
-    //         this.model.genres = [];
-    //     this.model.genres.push(genre);
-    // }
-
-    onFileSelected() {
-        const inputNode: any = document.querySelector('#file');
-
-        if (typeof (FileReader) !== 'undefined') {
-            const reader = new FileReader();
-
-            reader.onload = (e: any) => {
-                if (this.model.profilePicture)
-                    this.model.profilePicture.image = e.target.result;
-                else {
-                    this.model.profilePicture = new Photo;
-                    this.model.profilePicture.image = e.target.result;
-                }
-            };
-
-            reader.readAsDataURL(inputNode.files[0]);
-        }
+    selectedPerson(event: MatAutocompleteSelectedEvent): void {
+        this.personInput.nativeElement.value = '';
+        this.personsCtrl.setValue(null);
+        this.model = event.option.value;
+        this.canEdit = false;
     }
+
 }
 
 export interface DialogData {
