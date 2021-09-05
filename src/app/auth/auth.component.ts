d3df65f76503d77e518d17288e1d62a76fb4dfff
DVL_KinoMan <dvali.daviti@gmail.com>
2021-09-05 17:11:17 +0400

    Authorization Works

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,23 +1,118 @@
 import { Component, EventEmitter, Input, Output } from "@angular/core";
-import { FormControl, FormGroup } from "@angular/forms";
+import { AbstractControl, FormControl, FormGroup, FormGroupDirective, NgForm, ValidationErrors, ValidatorFn, Validators } from "@angular/forms";
+import { ErrorStateMatcher } from "@angular/material/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { MyErrorStateMatcher } from "../common/myErrorStateMatcher";
+import { AuthService } from "./auth.service";
 
 @Component({
     selector: "app-auth",
-    templateUrl: "./auth.component.html"
+    templateUrl: "./auth.component.html",
+    styleUrls: ['./auth.component.css']
 })
 
 export class AuthComponent {
     @Input() error: string | null;
     @Output() submitEM = new EventEmitter();
+    isLoading = false;
+    loginMode = true;
+    matcher = new MyErrorStateMatcher();
 
-    form: FormGroup = new FormGroup({
-        username: new FormControl(''),
-        password: new FormControl(''),
+    emailFormControl = new FormControl('', [
+        Validators.required,
+        Validators.email,
+    ]);
+
+    constructor(private authService: AuthService,
+        private router: Router,
+        private activatedRoute: ActivatedRoute) {
+
+    }
+    ngOnInit(): void {
+        this.activatedRoute.queryParams.subscribe(
+            params => {
+                var registerMode = params['registerMode'];
+                if (registerMode)
+                    this.loginMode = false;
+            }
+        );
+    }
+
+    checkPasswords: ValidatorFn = (group: AbstractControl): ValidationErrors | null => {
+        this.error = null;
+        let pass = group.get('password').value;
+        let confirmPass = group.get('confirmPassword').value;
+        if (pass === confirmPass)
+            return null;
+        this.error = "Passwords do not match";
+        return { notSame: true }
+    }
+
+    loginForm: FormGroup = new FormGroup({
+        username: new FormControl('', [Validators.required]),
+        password: new FormControl('', [Validators.required]),
     });
 
-    submit() {
-        if (this.form.valid) {
-            this.submitEM.emit(this.form.value);
-        }
+    registrationForm: FormGroup = new FormGroup({
+        username: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(50)]),
+        password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(50)]),
+        confirmPassword: new FormControl('', [Validators.required]),
+        email: new FormControl('', [Validators.required, Validators.email]),
+        birthDate: new FormControl('', [Validators.required])
+    }, { validators: this.checkPasswords });
+
+
+    onLoginSubmit() {
+        if (this.loginMode && !this.loginForm.valid)
+            return;
+
+        const userName = this.loginForm.value.username;
+        const password = this.loginForm.value.password;
+
+        this.isLoading = true;
+
+        this.authService.login(userName, password)
+            .subscribe(res => {
+                console.log(res);
+                this.isLoading = false;
+                this.activatedRoute.queryParams.subscribe(
+                    params => {
+                        var url = params['returnUrl'];
+                        if (url)
+                            this.router.navigate([url]);
+                        else this.router.navigate(["/films"]);
+                    }
+                );
+            }, error => {
+                console.log(error);
+                this.error = error;
+                this.isLoading = false;
+            });
+
+        this.loginForm.reset();
+    }
+
+    onRegisterSubmit() {
+        if (!this.loginMode && !this.registrationForm.valid)
+            return;
+
+        const userName = this.registrationForm.value.username;
+        const password = this.registrationForm.value.password;
+        const email = this.registrationForm.value.email;
+        const birthDate = this.registrationForm.value.birthDate;
+
+        this.isLoading = true;
+
+        this.authService.register(userName, password, email, birthDate)
+            .subscribe(res => {
+                console.log(res);
+                this.isLoading = false;
+                this.loginMode = true;
+                this.registrationForm.reset();
+            }, error => {
+                console.log(error);
+                this.error = error;
+                this.isLoading = false;
+            });
     }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,20 +1,91 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable, Subject, throwError } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
+import { User } from "./user.model";
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+    baseUrl = "https://localhost:44338";
+    user = new Subject<User>();
 
     constructor(private http: HttpClient) {
 
     }
 
-    register(email: string, password: string) {
+    register(userName: string, password: string, email: string, birthDate: Date): Observable<RegisterResponse> {
 
-        return this.http.post("ad",
+        return this.http.post<RegisterResponse>(this.baseUrl + "/Accounts/Register",
             {
+                userName: userName,
                 email: email,
                 password: password,
-            }
-        );
+                birthDate: birthDate
+            })
+            .pipe(
+                catchError(this.handleError),
+                tap(resData => {
+
+                })
+            );;
     }
+
+    login(userName: string, password: string): Observable<LoginResponse> {
+
+        return this.http.post<LoginResponse>(this.baseUrl + "/Accounts/Login", {
+            userName: userName,
+            password: password
+        })
+            .pipe(
+                catchError(this.handleError),
+                tap(resData => {
+                    this.handleAuthentication(
+                        resData.userName,
+                        resData.photo,
+                        resData.token,
+                        resData.expiration
+                    );
+                })
+            );
+    }
+
+    private handleAuthentication(
+        userName: string,
+        photo: string,
+        token: string,
+        expiration: Date
+    ) {
+        const user = new User(userName, photo, token, expiration);
+        this.user.next(user);
+    }
+
+    private handleError(errorRes: HttpErrorResponse) {
+        let errorMessage = 'An unknown error occurred!';
+        if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+        }
+        switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+                errorMessage = 'This email exists already';
+                break;
+            case 'EMAIL_NOT_FOUND':
+                errorMessage = 'This email does not exist.';
+                break;
+            case 'INVALID_PASSWORD':
+                errorMessage = 'This password is not correct.';
+                break;
+        }
+        return throwError(errorMessage);
+    }
+}
+
+export class LoginResponse {
+    userName: string;
+    photo?: string;
+    token: string;
+    expiration: Date
+}
+
+export class RegisterResponse {
+
 }
